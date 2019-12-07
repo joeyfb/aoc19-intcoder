4c2edfb6b7fedba3b57129efc39f32dd9c7487ce
Joey Bomber <joseph.bomber@gmail.com>
2019-12-07 15:08:10 -0800

    tests for input of format 7.1 work

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,50 @@ fn read(filename: &str) -> Result<Vec<i64>,std::io::Error> {
 
 fn main() -> io::Result<()> {
     let prog = read("program.txt")?;
+    
+    let answer = part1(&prog);
+    println!("MAXIMUM THRUST: {:?}", answer);
+
+    let answer = part2(&prog);
+    println!("FEEDBACK THRUST: {:?}", answer);
+
+    Ok(())
+}
+
+fn part1(prog: &Vec<i64>) -> i64 {
+    let mut phase_settings = vec!(0,1,2,3,4);
+    let mut answers = Vec::new();
+    let num_computers = phase_settings.len();
+
+    loop {
+        // permutohedron has to_vec generate next permutation
+        let phase = phase_settings.to_vec();
+        
+        let mut mcoder = multicoder::MultiCoder::new(&prog, num_computers);
+        
+        for p in phase {
+            mcoder.manual(p);
+        }
+
+        let mut answer = 0;
+        for _i in 0..num_computers {
+            answer = mcoder.manual(answer);
+        }
+
+        answers.push(answer);
+
+        // ask for next permutation
+        if !phase_settings.next_permutation() {
+            break;
+        }
+    }
+
+    answers.sort();
+
+    answers[answers.len() - 1]
+}
+
+fn part2(prog: &Vec<i64>) -> i64 {
     let mut phase_settings = vec!(5,6,7,8,9);
     let mut answers = Vec::new();
     let num_computers = phase_settings.len();
@@ -48,9 +92,7 @@ fn main() -> io::Result<()> {
 
     answers.sort();
 
-    println!("MAXIMUM THRUST: {:?}", answers[answers.len() - 1]);
-
-    Ok(())
+    answers[answers.len() - 1]
 }
 
 #[cfg(test)]
--- src/multicoder.rs
+++ src/multicoder.rs
@@ -55,6 +55,46 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_normal1() {
+        let prog = vec!(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0);
+        let phase = vec!(4,3,2,1,0);
+        let size = phase.len();
+        let mut mcoder = MultiCoder::new(&prog, size);
+
+        for p in phase {
+            mcoder.manual(p);
+        }
+
+        let mut answer = 0;
+        for _i in 0..size {
+            answer = mcoder.manual(answer);
+        }
+
+        assert_eq!(answer, 43210);
+    }
+
+    #[test]
+    fn test_normal2() {
+        let prog = vec!(3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
+                        1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0);
+        let phase = vec!(1,0,4,3,2);
+        let size = phase.len();
+        let mut mcoder = MultiCoder::new(&prog, size);
+
+        for p in phase {
+            mcoder.manual(p);
+        }
+
+        let mut answer = 0;
+        for _i in 0..size {
+            answer = mcoder.manual(answer);
+        }
+
+        assert_eq!(answer, 65210);
+    }
+
+
+    #[test]
     fn test_feedback1() {
         let prog = vec!(3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,
                         4,27,1001,28,-1,28,1005,28,6,99,0,0,5);

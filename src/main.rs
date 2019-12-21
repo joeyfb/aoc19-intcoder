10ba32ee0c4cb96b8716b7d66fc9a8eb20deb7d7
Joey Bomber <joseph.bomber@gmail.com>
2019-12-21 09:03:35 -0800

    removed asciicoder into own file and refactored

--- src/main.rs
+++ src/main.rs
@@ -10,111 +10,7 @@ use termion::raw::IntoRawMode;
 use termion::{clear};
 
 mod intcoder;
-
-#[derive(Debug)]
-enum Tile {
-    Scaffold,       // 35 - #
-    Space,          // 46 - .
-    NewLine,        // 10 - \n 
-    Robot(u8),      // 118 | 60 | 61 | 94
-    Message(u8),    // Anything else positive
-    Halt,
-    Input
-}
-
-impl Tile {
-    pub fn new(response: intcoder::IntResponse) -> Tile {
-        let t = match response {
-            intcoder::IntResponse::Output(i) => i,
-            intcoder::IntResponse::Halt => -2,
-            intcoder::IntResponse::Input => return Tile::Input,
-            _ => return Tile::Halt,
-        };
-
-        match t {
-            10 => Tile::NewLine,
-            46 => Tile::Space,
-            35 => Tile::Scaffold,
-            118 | 60 | 61 | 94 => Tile::Robot(t as u8),
-            _ => Tile::Message(t as u8),
-        }
-    }
-
-    pub fn toChar(&self) -> char {
-        'a'
-    }
-}
-
-
-
-fn game(icoder : &mut intcoder::Intcode) -> bool {
-    let mut rows = Vec::new();
-    let mut row = Vec::new();
-    let mut message = Vec::new();
-
-    loop {
-        let r = icoder.run(0);
-
-        let t = match Tile::new(r) {
-            Tile::Scaffold => '#',
-            Tile::Space => '.',
-            Tile::Robot(i) => i as char,
-            Tile::Input => {
-                println!("waiting for input!");
-                continue;
-            },
-            Tile::NewLine => {
-                rows.push(row);
-                row = Vec::new();
-                continue;
-            },
-            Tile::Message(i) => {
-                message.push(i as char);
-                continue;
-            },
-
-            Tile::Halt => break
-        };
-
-        row.push(t);
-    }
-
-    // numbering
-    for i in 0..45 {
-        if i < 10 {
-            print!(" ");
-        } else {
-            print!("{}", i/10);
-        }
-    }
-    println!();
-    for i in 0..5 {
-        for j in 0..10 {
-            print!("{}", j);
-
-            if i == 4 && j == 5 {
-                break;
-            }
-        }
-    }
-    println!("");
-
-    // graph printing
-    for (i, r) in rows.iter().enumerate() {
-        for (j, t) in r.iter().enumerate() {
-            print!("{}", t);
-        }
-        println!("{}", i);
-    }
-
-    // message printing
-    for c in message {
-        print!("{}", c);
-    }
-    println!("");
-
-    true
-}
+mod asciicoder;
 
 
 fn main() -> io::Result<()> {
@@ -122,7 +18,7 @@ fn main() -> io::Result<()> {
     let prog = read("program.txt")?; 
     let mut computer = intcoder::Intcode::new(&prog);
 
-    game(&mut computer);
+    asciicoder::game(&mut computer);
 
     // TIMING
     let duration = (now.elapsed().subsec_millis() as u128) + 1000*(now.elapsed().as_secs() as u128);

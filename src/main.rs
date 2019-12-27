a77d898a8974c0c8f0d3541d2fbd5d2cd0140c45
Joey Bomber <joseph.bomber@gmail.com>
2019-12-27 09:52:51 -0800

    very slow manual solve for part 1

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,51 @@ use std::time;
 mod intcoder;
 mod board;
 
+
 pub fn game(icoder : &mut intcoder::Intcode) -> bool {
     let mut last = -1;
+    let mut last_coor = (0, 0);
+    let size = 60;
+    let mut map: Vec<Vec<char>> = Vec::new();
+    let mut coor = (size/2,size/2);
+
+    for i in 0..size {
+        let mut row = Vec::new();
+        for j in 0..size {
+            row.push(' ');
+        }
+        map.push(row);
+    }
 
     loop {
         match icoder.run() {
             intcoder::IntResponse::Output(i) => {
                 last = i;
-                let c = i as u8;
-                print!("{}", c as char);
+                println!("output: {}", i);
+                print_map(&mut map, &mut coor, i, &last_coor);
             },
-
             intcoder::IntResponse::Input => {
                 let input: i64 = std::io::stdin()
                     .bytes() 
                     .next()
                     .and_then(|result| result.ok())
                     .map(|byte| byte as i64).unwrap();
-                icoder.input(input);
+
+                if input == 10 { continue; }
+
+                let val = input - 48;
+                last_coor = (coor.0, coor.1);
+
+                match val {
+                    1 => { coor = (coor.0, coor.1 + 1) },
+                    2 => { coor = (coor.0, coor.1 - 1) },
+                    3 => { coor = (coor.0 - 1, coor.1) },
+                    4 => { coor = (coor.0 + 1, coor.1) },
+                    _ => continue
+                };
+
+                icoder.input(val);
+
                 continue;
             },
             intcoder::IntResponse::Halt => break,
@@ -33,6 +60,34 @@ pub fn game(icoder : &mut intcoder::Intcode) -> bool {
     println!("{}", last);
 
     true
+}
+
+fn print_map(map: &mut Vec<Vec<char>>, coor: &mut (usize, usize), i: i64, last_coor: &(usize, usize)) {
+    let size = map.len();
+
+    match i {
+        0 => { 
+            map[coor.1][coor.0] = '#';
+            coor.0 = last_coor.0;
+            coor.1 = last_coor.1;
+        },
+        1 => { map[coor.1][coor.0] = '.' },
+        2 => { map[coor.1][coor.0] = 'W' },
+        _ => panic!("weird output value! {}", i)
+    }
+
+    for (y, line) in map.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if x == coor.0 && y == coor.1 {
+                print!("D");
+            } else if x == size/2 && y == size/2 {
+                print!("S");
+            } else {
+                print!("{}", c);
+            }
+        }
+        println!("");
+    }
 }
 
 
@@ -66,6 +121,7 @@ fn read(filename: &str) -> Result<Vec<i64>,std::io::Error> {
         }
     }).collect())
 }
+
 
 #[cfg(test)]
 mod tests {

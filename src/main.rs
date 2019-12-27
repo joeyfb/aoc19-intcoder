0c65be0564ea684b12068018e42dd9a0095f76b6
Joey Bomber <joseph.bomber@gmail.com>
2019-12-26 22:42:07 -0800

    finished part 1, manually

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, prelude::*};
+use std::io::{self, Read, prelude::*};
 use std::time::{Instant};
 use std::time;
 
@@ -7,34 +7,9 @@ mod intcoder;
 mod board;
 
 pub fn game(icoder : &mut intcoder::Intcode) -> bool {
-    let comma = ',' as u8;
-    let mut isComma = false;
-
-    let mut input = vec!(
-        // main
-        'A', 'A', 'C', 'B', 'B', 'A', 'B', 'C', 'B', 'C', '\n',
-
-        // A                     <||
-        'L', '4', 'L', '4', 'L', '6', 'R', '1', '0', 'L', '6', '\n', 
-
-        // B            ||>
-        'R', '8', 'R', '1', '0', 'L', '6', '\n', 
-
-        // C
-        'L', '1', '2', 'L', '6', 'R', '1', '0', 'L', '6', '\n',
-
-        // Videofeed
-        'N', '\n',
-        // Padding
-        '1', '\n',
-    );
-    let mut last = 1;
+    let mut last = -1;
 
     loop {
-        if input[0] == '\n' {
-            isComma = false;
-        }
-
         match icoder.run() {
             intcoder::IntResponse::Output(i) => {
                 last = i;
@@ -43,22 +18,12 @@ pub fn game(icoder : &mut intcoder::Intcode) -> bool {
             },
 
             intcoder::IntResponse::Input => {
-                let i = match isComma {
-                    true => comma,
-                    false => input.remove(0) as u8
-                };
-
-                print!("{}", i as char);
-                icoder.input(i as i64);
-
-                if i as char != '\n' {
-                    isComma =  ! isComma;
-                }
-
-                if (i as char).is_digit(10) && input[0].is_digit(10) {
-                    isComma = false;
-                }
-
+                let input: i64 = std::io::stdin()
+                    .bytes() 
+                    .next()
+                    .and_then(|result| result.ok())
+                    .map(|byte| byte as i64).unwrap();
+                icoder.input(input);
                 continue;
             },
             intcoder::IntResponse::Halt => break,
